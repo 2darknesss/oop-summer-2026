5f8a0a87b84660235f75c92ade8c6323d36bf559
2darknesss <assassin.dn12345@gmail.com>
2026-03-26 10:24:14 +0000

    test 4

---
+++ Dmytro Simonov/36198/lecture_4_test.py
@@ -0,0 +1,24 @@
+amount_of_spicy_adana_kebabs_in_order = 12
+total_weight_of_fresh_pistachio_baklava = 5.5
+number_of_cold_ayran_glasses = 4
+pieces_of_sweet_rahat_lokum = 20
+price_for_one_large_lahmacun = 15
+
+class TraditionalTurkishRestaurant:
+    pass
+
+class DeliciousIskenderKebab:
+    pass
+
+class HomemadeLentilSoupMercimek:
+    pass
+
+class SweetCheeseKunefeDessert:
+    pass
+
+MAX_OVEN_TEMPERATURE_FOR_PIDE = 300
+DEFAULT_DELIVERY_TIME_MINUTES = 45
+MINIMUM_ORDER_FOR_FREE_BAKLAVA = 100
+MAXIMUM_SPICE_LEVEL_FOR_KEBAB = 10
+
+"ultimate-turkish-kebab-and-baklava-repo"
